bd2cc272599ba0e93c6e1de8f7f8ac2a402af084
Dominik Szilvasi <dominik.szilvasi.2024@mumail.ie>
2026-04-19 17:59:26 +0100

    Switch app shell to theme stylesheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import './App.css';
+import './theme.css';
 
 import DashboardPage from './pages/DashboardPage';
 import HistoryPage from './pages/HistoryPage';
